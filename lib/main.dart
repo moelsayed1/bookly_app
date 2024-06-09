05af9091d002ef30cf8a3c3b417b38565dfc77fe
moelsayed1 <mostafaembaby19@gmail.com>
2024-06-09 21:38:24 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/Features/Splash/Presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Scaffold(
+        body: SplashView(),
+      ),
+    );
+  }
+}
+
+
+
