33b9af25f45582d19c6d1fa111005b3a4e1ec5c1
moelsayed1 <mostafaembaby19@gmail.com>
2024-06-09 21:40:02 +0300

    create splash view body

--- lib/Features/Splash/Presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/views/widgets/splash_view_body.dart
@@ -5,6 +5,10 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Column(
+      children: [
+
+      ],
+    );
   }
 }
