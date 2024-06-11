34cf396e7205c82971f8ac901eccaf49ed1614d3
moelsayed1 <mostafaembaby19@gmail.com>
2024-06-11 17:54:24 +0300

    create app style

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,40 @@ class HomeViewBody extends StatelessWidget {
             padding: const EdgeInsets.only(left: 8),
             child: Text(
               'Best Seller',
-              style: Style.titleMedium,
+              style: Style.textStyle18,
+            ),
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: AspectRatio(
+              aspectRatio: 2.3 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: DecorationImage(
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
@@ -30,3 +64,4 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
+
--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff150526);
 
 const kTransitionDuration = Duration(microseconds: 250);
+
+const kGtSectraFine = 'GT Sectra Fine';
