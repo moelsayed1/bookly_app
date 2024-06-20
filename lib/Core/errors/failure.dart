ea9c670efcd7eded3b6878f8dfe4fc8a91a610ff
moelsayed1 <mostafaembaby19@gmail.com>
2024-06-20 21:24:50 +0300

    implement fetch featured books

--- lib/Core/errors/failure.dart
+++ lib/Core/errors/failure.dart
@@ -1,3 +1,54 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+
+  final String errMessage;
+
+  const Failure(this.errMessage);
+
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+
+    switch(dioException.type) {
+
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with API server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with API server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with API server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate with API server');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Your request is cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection error with API server');
+      case DioExceptionType.unknown:
+        if(dioException.message!.contains('SocketException')) {
+          return ServerFailure('No internet connection');
+        }
+        return ServerFailure('Unknown error, Please try again!');
+      default:
+        return ServerFailure('Something went wrong, Please try again!');
+    }
+  }
+  
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    
+    if(statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    }else if(statusCode == 404) {
+      return ServerFailure('Your request not found, Please try later!');
+    }else if(statusCode == 500) {
+      return ServerFailure('Internal server error, Please try later!');
+    }else {
+      return ServerFailure('Something went wrong, Please try again!');
+    }
+  }
+}
--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -3,33 +3,66 @@ import 'package:bookly_app/Core/utils/api_service.dart';
 import 'package:bookly_app/Features/home/data/models/book_model.dart';
 import 'package:bookly_app/Features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
-
   final ApiService apiService;
 
   HomeRepoImpl(this.apiService);
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
-   try {
-     var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest');
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest');
 
-     List<BookModel> books = [];
+      List<BookModel> books = [];
 
-     for(var item in data['items']) {
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
 
-       books.add(BookModel.fromJson(item));
-     }
-
-     return right(books);
-   } catch (e) {
-     return left(ServerFailure());
-   }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+          'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
